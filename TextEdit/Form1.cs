8bbcc13a6dc501ea29bdea01f7be546b915b0689
CentyPoo <centypoo@hotmail.co.uk>
2015-09-04 17:38:39 +0100

    Added closing tab

--- TextEdit/Form1.cs
+++ TextEdit/Form1.cs
@@ -18,6 +18,7 @@ namespace TextEdit
         {
             InitializeComponent();
             this.KeyDown += new KeyEventHandler(Form_KeyDown);
+            this.tabControl1.MouseClick += tabControl1_MouseClick;
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
@@ -39,19 +40,27 @@ namespace TextEdit
             saveFile();
         }
 
-        void Form_KeyDown(object sender, KeyEventArgs e)
-        {
-            if(e.Control && e.KeyCode == Keys.S)
-            {
-                saveFile();
-            }
-        }
-
         void saveFile()
         {
             var files = new Files();
             RichTextBox rtb = (RichTextBox)tabControl1.SelectedTab.Tag;
             files.saveFile(tabControl1.SelectedTab.Text, rtb.Lines);
+        }
+
+        void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                saveFile();
+            }
+        }
+
+        void tabControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle)
+            {
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+            }
         }
     }
 }

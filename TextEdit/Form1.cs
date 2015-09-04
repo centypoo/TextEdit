3d19ed6fb5b66ee23ad3e929ecf49811084e1519
CentyPoo <centypoo@hotmail.co.uk>
2015-09-04 15:51:25 +0100

    Starting out

---
+++ TextEdit/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace TextEdit
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var tabs = new Tabs();
+            tabs.openTab();
+            tabControl1.TabPages.Add(tabs.tabsOpen[tabs.amountOfForms]);
+            tabControl1.SelectedTab = tabs.tabsOpen[tabs.amountOfForms];
+            tabs.amountOfForms += 1;
+        }
+    }
+}
---
+++ TextEdit/Tabs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace TextEdit
+{
+    class Tabs
+    {
+        public int amountOfForms = 0;
+        public TabPage[] tabsOpen = new TabPage[255];
+        public void openTab()
+        {
+            Form1 frm1 = new Form1();
+
+            OpenFileDialog fd = new OpenFileDialog();
+            DialogResult userClickedOK = fd.ShowDialog();
+
+            if (userClickedOK == DialogResult.OK)
+            {
+                RichTextBox tmp = new RichTextBox();
+                tmp.Lines = File.ReadAllLines(fd.FileName, Encoding.Default);
+                tmp.Dock = DockStyle.Fill;
+                tmp.BackColor = SystemColors.ControlDarkDark;
+                tmp.ForeColor = Color.White;
+                tmp.WordWrap = false;
+                tmp.Font = new Font("Arial", 10);
+
+                TabPage tb = new TabPage(fd.FileName);
+                tabsOpen[amountOfForms] = tb;
+
+                //frm1.tabControl1.TabPages.Add(this.tabsOpen[amountOfForms]);
+                tabsOpen[amountOfForms].Controls.Add(tmp);
+
+                //frm1.tabControl1.SelectedTab = this.tabsOpen[amountOfForms];
+            }
+        }
+    }
+}
